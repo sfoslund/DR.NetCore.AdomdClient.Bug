3f0fb4bb2c79cbe4e1a59a24831ad428fba8bb91
Dmitriy Reshetov <dreshetov@eastbanctech.com>
2020-10-01 13:42:02 -0400

    initial commit

---
+++ DR.NetCore.AdomdClient.Bug/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AnalysisServices.AdomdClient;
+using System;
+
+namespace DR.NetCore.AdomdClient.Bug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdomdConnection conne = new AdomdConnection();
+            Console.WriteLine($"Hello World! {conne.ToString()}");
+        }
+    }
+}
